126aeed7d4175cc275de46cd8a21d825a3d6f526
Paul Vauterin <pvaut@gmail.com>
2013-03-05 16:43:55 +0000

    Basic integrity check for file names

--- responders/getfile.py
+++ responders/getfile.py
@@ -2,7 +2,11 @@ import base64
 import config
 
 def response(returndata):
-    f=open(config.BASEDIR+'/'+returndata['name']+'.txt')
+    filename=returndata['name']
+    #!!!todo: add more integrity checks for the filename
+    if filename.find('..')>=0:
+        raise Exception('Invalid file name')
+    f=open(config.BASEDIR+'/'+filename+'.txt')
     content=f.read()
     f.close()
     returndata['content']=base64.b64encode(content)
