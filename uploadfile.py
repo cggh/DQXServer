d6bbe19d062d8a87618e8e8b481b516b5043db8f
Paul Vauterin <paul.vauterin@gmail.com>
2013-07-04 15:44:12 +0100

    File upload

---
+++ uploadfile.py
@@ -0,0 +1,53 @@
+import cgi
+import os
+import sys
+import config
+import uuid
+
+#Based on https://github.com/thejimmyg/wsgi-file-upload
+
+def response(environ):
+
+
+    post = cgi.FieldStorage(
+        fp=environ['wsgi.input'],
+        environ=environ,
+        keep_blank_values=True
+    )
+
+    #if 'filedata' not in post:
+    #    return 'missing filedata'
+
+    #fileitem = post["filedata"]
+    fileitem=post#Note: if file is send directly using XMLHttpRequest, this is the way to read it
+
+
+    if fileitem.file:
+        #filename = fileitem.filename.decode('utf8').replace('\\','/').split('/')[-1].strip()
+        #if not filename:
+        #    raise Exception('No valid filename specified')
+        filename = str(uuid.uuid1())
+        file_path = os.path.join(config.BASEDIR, 'Uploads', filename)
+        # Using with makes Python automatically close the file for you
+        counter = 0
+        with open(file_path, 'wb') as output_file:
+            # In practice, sending these messages doesn't work
+            # environ['wsgi.errors'].write('Receiving upload ...\n')
+            # environ['wsgi.errors'].flush()
+            # print 'Receiving upload ...\n'
+            while 1:
+                data = fileitem.file.read(1024)
+                # End of file
+                if not data:
+                    break
+                output_file.write(data)
+                counter += 1
+                if counter == 100:
+                    counter = 0
+                    # environ['wsgi.errors'].write('.')
+                    # environ['wsgi.errors'].flush()
+                    # print '.',
+
+    print('Uploaded file '+filename)
+    response='filename='+filename
+    return str(response)
--- wsgi_server.py
+++ wsgi_server.py
@@ -2,11 +2,34 @@ from urlparse import parse_qs
 import DQXUtils
 import simplejson
 import responders
+import uploadfile
 
 
 def application(environ, start_response):
     returndata = dict((k,v[0]) for k,v in parse_qs(environ['QUERY_STRING']).items())
+    print('REQUEST: '+str(environ))
+
+    if 'datatype' not in returndata:
+        print('--> request does not contain datatype')
+        status = '404 NOT FOUND'
+        response=''
+        response_headers = [('Content-type', 'application/json'),
+                            ('Content-Length', str(len(response)))]
+        start_response(status, response_headers)
+        yield uploadfile.response(environ)
+        return
+
     request_type = returndata['datatype']
+
+    if request_type == 'uploadfile':
+        response = uploadfile.response(environ)
+        status = '200 OK'
+        response_headers = [('Content-type', 'application/json'),
+                            ('Content-Length', str(len(response)))]
+        start_response(status, response_headers)
+        yield response
+        return
+
 
     tm = DQXUtils.Timer()
     try:
@@ -17,13 +40,16 @@ def application(environ, start_response):
 
     #todo: make the response handling part of the handler, to avoid this branching
     #This will become necessary when we have more handlers with different response types (e.g. other downloads)
+    handled=False
     if request_type == "downloadtable":#Respond to a download request with a text attachment
         status = '200 OK'
         response_headers = [('Content-type', 'text/plain'),('Content-Disposition','attachment; filename=download.txt')]
         start_response(status, response_headers)
         for item in response:
             yield item
-    else:#respond to any other event with json
+        handled=true
+
+    if not(handled):#respond to any other event with json
         response = simplejson.dumps(response, use_decimal=True)
         status = '200 OK'
         response_headers = [('Content-type', 'application/json'),
