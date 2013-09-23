d34a45f64c3457ee72d3e986b9781e0beb1a1a58
pvaut <paul.vauterin@gmail.com>
2013-09-23 13:52:53 +0100

    Added comments

--- config.py
+++ config.py
@@ -1,12 +1,30 @@
 
-#Configuration parameters for connecting to the data
-#todo: for production server data, this should be moved to a safer place outside the source code
+###########################################################################################################
+# Configuration parameters for connecting to the MySQL database
+###########################################################################################################
+
+# Machine name where the database is running
 DBSRV = 'localhost'
+
+# Login name used to gain access
 DBUSER = 'root'
+
+# Password used to gain access
+#todo: for production server data that also includes confidential information, this should be moved to a safer place outside the source code
 DBPASS = '1234'
+
+# Default database name (e.g. used to store certain settings)
+DB='datasetindex'
+
+# Command to invoke the MySQL prompt
+mysqlcommand = '/usr/local/mysql/bin/mysql'
+
+###########################################################################################################
+# Server side file structure location
+###########################################################################################################
+
+# Root directory of the server side file structure
 BASEDIR = '/Users/pvaut/Documents/Genome'
 #BASEDIR = '/mnt/storage/webapps'
 
-DB='datasetindex'
 
-mysqlcommand = '/usr/local/mysql/bin/mysql'
