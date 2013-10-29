eca29362962af83790c25a38078a1dbf93b3981a
Paul Vauterin <paul.vauterin@gmail.com>
2013-10-29 16:12:05 +0000

    Get record count

---
+++ responders/getrecordcount.py
@@ -0,0 +1,36 @@
+import B64
+import DQXDbTools
+import DQXUtils
+
+
+
+def response(returndata):
+    mytablename=returndata['tbname']
+    encodedquery=returndata['qry']
+
+    databaseName=None
+    if 'database' in returndata:
+        databaseName = returndata['database']
+    db = DQXDbTools.OpenDatabase(databaseName)
+    cur = db.cursor()
+
+    whc=DQXDbTools.WhereClause()
+    whc.ParameterPlaceHolder='%s'#NOTE!: MySQL PyODDBC seems to require this nonstardard coding
+    whc.Decode(encodedquery)
+    whc.CreateSelectStatement()
+
+
+    #Determine total number of records
+    sqlquery="SELECT COUNT(*) FROM {0}".format(mytablename)
+    if len(whc.querystring_params) > 0:
+        sqlquery += " WHERE {0}".format(whc.querystring_params)
+    print('   executing count query...')
+    tm = DQXUtils.Timer()
+    cur.execute(sqlquery, whc.queryparams)
+    print('   finished in {0}s'.format(tm.Elapsed()))
+    returndata['TotalRecordCount'] = cur.fetchone()[0]
+
+    cur.close()
+    db.close()
+
+    return returndata
