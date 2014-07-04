3d414a7bc4b5e6c6c2b1ec8f44b69bab537fd50d
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-07-04 13:40:40 +0100

    Allow URLS to have dataset names in

--- wsgi_general.py
+++ wsgi_general.py
@@ -1,5 +1,5 @@
 import DQXUtils
-
+import DQXDbTools
 
 def application(environ, start_response):
     #For the root we do a relative redirect to index.html, hoping the app has one
@@ -7,6 +7,20 @@ def application(environ, start_response):
         start_response('301 Moved Permanently', [('Location', 'index.html'),])
         return
 
+    with DQXDbTools.DBCursor() as cur:
+        cur.execute('select id from datasetindex')
+        datasets = [d[0] for d in cur.fetchall()]
+
+    #Redirect to specific dataset
+    path = environ['PATH_INFO'].split('/')
+    if len(path) >= 2 and path[-2] in datasets:
+        start_response('301 Moved Permanently', [('Location', '../index.html?dataset='+path[-2]),])
+        return
+    if path[-1] in datasets:
+        start_response('301 Moved Permanently', [('Location', '../index.html?dataset='+path[-1]),])
+        return
+
+    #Everything else is 404
     DQXUtils.LogServer('404:' + environ['PATH_INFO'])
     start_response('404 Not Found', [])
     try:
