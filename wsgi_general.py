41431ed7428f7884c6e36631afe4a0e6d7a85591
Ben Jeffery <ben.jeffery@gmail.com>
2016-05-24 21:21:53 +0100

    Add handling for panoptes 2 session ids

--- wsgi_general.py
+++ wsgi_general.py
@@ -1,5 +1,9 @@
 import DQXUtils
 import DQXDbTools
+import re
+import os
+session_id_regex = re.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", re.IGNORECASE)
+static_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
 
 def application(environ, start_response):
     #For the root we do a relative redirect to index.html, hoping the app has one
@@ -20,11 +24,18 @@ def application(environ, start_response):
         start_response('301 Moved Permanently', [('Location', '../index.html?dataset='+path[-1]),])
         return
 
+    #Session ids return index.html
+    if len(path) > 0 and session_id_regex.match(path[-1]):
+        start_response('200 OK', [])
+        with open(os.path.join(static_path, 'index.html')) as page:
+            yield page.read()
+        return
+
     #Everything else is 404
     DQXUtils.LogServer('404:' + environ['PATH_INFO'])
     start_response('404 Not Found', [])
     try:
-        with open('static/404.html') as page:
+        with open(os.path.join(static_path, '404.html')) as page:
             yield page.read()
     except IOError:
         yield '404 Page Not Found'
